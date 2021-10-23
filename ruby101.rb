7ee59de1873344dc36596d0622773f0a46bc1c52
ls <cancel>
2021-10-23 11:16:56 -0500

    loops

--- ruby101.rb
+++ ruby101.rb
@@ -12,8 +12,24 @@
 # world = "world"
 # puts "#{hello} #{world}"
 
-if 5 > 6
-    puts "true"
-else
-    puts "false"
+# #conditional if else statement
+# if 5 > 6
+#     puts "true"
+# else
+#     puts "false"
+# end
+
+
+#loop
+x = 0
+while x < 10
+    puts x
+    x += 1
 end
+
+y = 20
+until y < 10
+    puts y
+    y -= 1
+end
+

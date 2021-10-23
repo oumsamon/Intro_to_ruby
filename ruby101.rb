71dc6aa4b33ae6527094c068f4197b34cde86666
ls <cancel>
2021-10-23 11:12:47 -0500

    if statment

--- ruby101.rb
+++ ruby101.rb
@@ -1,13 +1,19 @@
-puts "Hello World"
+# puts "Hello World"
 
-# ruby filename.rb in terminal to run.
+# # ruby filename.rb in terminal to run.
 
-choose = "gouda"
-puts choose
+# choose = "gouda"
+# puts choose
 
-age = 38
-puts age
+# age = 38
+# puts age
 
-hello = "hello"
-world = "world"
-puts "#{hello} #{world}"
+# hello = "hello"
+# world = "world"
+# puts "#{hello} #{world}"
+
+if 5 > 6
+    puts "true"
+else
+    puts "false"
+end

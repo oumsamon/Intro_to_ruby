111146da02fcbb07a5f608869f6c165da72e3e26
ls <cancel>
2021-10-23 11:19:39 -0500

    array

--- ruby101.rb
+++ ruby101.rb
@@ -12,6 +12,7 @@
 # world = "world"
 # puts "#{hello} #{world}"
 
+
 # #conditional if else statement
 # if 5 > 6
 #     puts "true"
@@ -20,16 +21,21 @@
 # end
 
 
-#loop
-x = 0
-while x < 10
-    puts x
-    x += 1
-end
+# #loop
+# x = 0
+# while x < 10
+#     puts x
+#     x += 1
+# end
 
-y = 20
-until y < 10
-    puts y
-    y -= 1
-end
+# y = 20
+# until y < 10
+#     puts y
+#     y -= 1
+# end
 
+#array - puts = p
+myArr = [1, 2, 3, 4, 5]
+p myArr[0]
+p myArr.methods
+#methods show how many ways to do within the array.
